c74da63976e9baaac68ea1ed94e47833ea2d226d
Nolan Landefeld <nlandefeld1995@gmail.com>
2021-09-30 18:56:45 -0600

    create init layout (#4)
    * create init layout
    
    Created initial layouts
    Initial layout sucks!
    The resize works great left/right except for last div. Need to find a way to set max/min width on right side. Need to translate that size into what separation border is.
    
    * simple updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import './App.css';
+import * as React from "react";
+import * as commonStyles from "./common/styles/common.module.css";
+import * as appStyles from "./App.css";
+import * as classnames from "classnames/bind";
+import { ResizableDivWidth } from "./common/components/resizableComponents/ResizableDivWidth";
+import { ResizableDivHeight } from "./common/components/resizableComponents/ResizableDivHeight";
+const cx = classnames.bind({ ...commonStyles, ...appStyles });
 
 function App() {
   return (
-    <div className="App">
-      dndTracker
-    </div>
+    <>
+      <div className={cx("top-bar-div")}>
+        <div>setting</div>
+        <div>PlayerInfo</div>
+        <div>View Select</div>
+        <div>QuickLinks</div>
+      </div>
+
+      <div className={cx("main-container")}>
+        <ResizableDivWidth
+          inputWidth="25vw"
+          innerDiv={<div className={cx("left-container")}>Left</div>}
+        />
+
+        <ResizableDivWidth
+          inputWidth="50vw"
+          innerDiv={<div className={cx("middle-container")}>MiddleBar</div>}
+        />
+        <div className={cx("right-container")}>
+          <ResizableDivHeight
+            inputHeight="50vh"
+            innerDiv={
+              <div className={cx("right-container-one")}>rightbar1</div>
+            }
+          />
+
+          <div className={cx("right-container-two")}>rightbar2</div>
+        </div>
+      </div>
+    </>
   );
 }
 
