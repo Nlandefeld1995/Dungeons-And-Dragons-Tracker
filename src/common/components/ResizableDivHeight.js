96a872afba04ce7f137de15f2c3601994aff59df
Nolan Landefeld <nlandefeld1995@gmail.com>
2021-09-30 18:38:15 -0600

    create init layout
    Created initial layouts
    Initial layout sucks!
    The resize works great left/right except for last div. Need to find a way to set max/min width on right side. Need to translate that size into what separation border is.

---
+++ src/common/components/ResizableDivHeight.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+import * as commonStyles from "../styles/common.module.css";
+import * as classnames from "classnames/bind";
+const cx = classnames.bind(commonStyles);
+
+export const ResizableDivHeight = ({ innerDiv, inputHeight }) => {
+  const [initialPos, setInitialPos] = React.useState(null);
+  const [initialSize, setInitialSize] = React.useState(null);
+  const [height, setHeight] = React.useState(inputHeight)
+
+  const initial = (e) => {
+    setInitialPos(e.clientY);
+    setInitialSize(e.target.parentElement.children[0].offsetHeight);
+    setHeight(`${e.target.parentElement.children[0].offsetHeight}px`)
+  };
+
+  const resize = (e) => {
+    let resizable = e.target.parentElement.children[0]
+    resizable.style.height = (`${
+      parseInt(initialSize) + parseInt(e.clientY - initialPos)
+    }px`);
+  };
+
+  return (
+    <div className={cx("BlockHeight")}>
+      <div className={cx("ResizableHeight")}
+      style={{height: `${height}`}}>{innerDiv}</div>
+      <div
+        className={cx("DraggableHeight")}
+        draggable="true"
+        onDragStart={(e)=>initial(e)}
+        onDrag={(e)=>resize(e)}
+      />
+    </div>
+  );
+};
