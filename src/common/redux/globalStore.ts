acfd5c8cd0cddee3e9b1b596d71eae5b53adb377
Nolan Landefeld <nlandefeld1995@gmail.com>
2023-01-23 17:49:54 -0700

    Create base router (#6)

--- src/common/redux/globalStore.ts
+++ src/common/redux/globalStore.ts
@@ -1,30 +1,19 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { View } from "../interfaces/View";
 import State from "./state";
 
 interface storeState {
-  view: View;
 }
-const defaultState = (): storeState => {
-  return {
-    view: "home",
-  };
-};
+const initialState = 
+{};
 
 const GlobalSlice = createSlice({
   name: "global",
-  initialState: defaultState(),
+  initialState: initialState,
   reducers: {
-    UPDATE_VIEW: (state, { payload }: PayloadAction<View>) => {
-      state.view = payload;
-      if(payload === 'home'){
-        state = defaultState()
-      }
-    }
+   
   },
 });
 
-export const StoreView = (state: State) => state.GlobalState.view;
 
 export const ACTIONS = GlobalSlice.actions;
 export default GlobalSlice.reducer;
